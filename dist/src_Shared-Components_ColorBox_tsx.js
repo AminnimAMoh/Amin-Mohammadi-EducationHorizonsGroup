6870f748cb28746240d7b429315ce66d8382b954
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-09-29 15:28:19 +1000

    Spiral design in absolute positions.

--- dist/src_Shared-Components_ColorBox_tsx.js
+++ dist/src_Shared-Components_ColorBox_tsx.js
@@ -25,16 +25,16 @@ function ColorBox({
   index
 }) {
   const angle = 2 * Math.PI / 32768 * index;
-  const radius = 50 * Math.cos(index);
+  const radius = 100 * Math.sin(index);
   const x = radius * Math.cos(angle);
   const y = radius * Math.sin(angle);
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("span", {
     className: "container_colorBox",
     style: {
       backgroundColor: `rgba(${red}, ${green}, ${blue}, 1)`,
-      transform: `translate(${x * 50}%,${y * 50}%)`,
-      width: `${width * 2}%`,
-      height: `${height * 2}%`
+      transform: `translate(${-50 + x * 100}%,${-50 + y * 100}%)`,
+      width: `${width / 2}%`,
+      height: `${height / 2}%`
     }
   });
 }
--- src/Shared-Components/ColorBox.tsx
+++ src/Shared-Components/ColorBox.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 function ColorBox({ red, green, blue, width, height, index }: Props): ReactElement {
   const angle=((2*Math.PI)/32768)*index;
-  const radius=50*Math.cos(index);
+  const radius=100*Math.sin(index);
   const x=radius*Math.cos(angle);
   const y=radius*Math.sin(angle);
   return (
@@ -19,9 +19,9 @@ function ColorBox({ red, green, blue, width, height, index }: Props): ReactEleme
       className="container_colorBox"
       style={{
         backgroundColor: `rgba(${red}, ${green}, ${blue}, 1)`,
-        transform: `translate(${x*50}%,${y*50}%)`,
-        width: `${width*2}%`,
-        height: `${height*2}%`,
+        transform: `translate(${-50+x*100}%,${-50+y*100}%)`,
+        width: `${width/2}%`,
+        height: `${height/2}%`,
       }}
     />
   );
